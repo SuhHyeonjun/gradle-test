b3f72c72c0b9ab3a9fd72f32885538b56727f2ea
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-01-31 22:54:12 +0900

    feat : 업데이트문 추가

--- src/main/java/org/example/service/posts/PostsService.java
+++ src/main/java/org/example/service/posts/PostsService.java
@@ -2,8 +2,11 @@ package org.example.service.posts;
 
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.example.domain.posts.Posts;
 import org.example.domain.posts.PostsRepository;
+import org.example.web.dto.PostsResponseDto;
 import org.example.web.dto.PostsSaveRequestDto;
+import org.example.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor // 생성자를 생성하게 하면 Autowired와 동일한 효과를 볼 수 있다.
@@ -14,5 +17,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    // entity 객체의 값만 변경하여도 별도로 Update쿼리를 날릴 필요가 없다. (더티 체킹)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
     }
 }
