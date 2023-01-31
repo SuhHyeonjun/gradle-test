eccb892006a58dd515bde9d4c489f1a026a69ec0
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-01-31 23:11:29 +0900

    chore : Update할 경우 주의사항 주석 추가

--- src/main/java/org/example/web/dto/PostsUpdateRequestDto.java
+++ src/main/java/org/example/web/dto/PostsUpdateRequestDto.java
@@ -15,4 +15,7 @@ public class PostsUpdateRequestDto {
         this.title = title;
         this.content = content;
     }
+
+    // Update는 쿼리를 날릴필요없이 Entity 객체의 값만 변경하면 트랜잭션이 끝나는 시점에 해당 테이블에 변경사항을 반영한다.
+    // PostsService의 update() 메소드를 확인
 }
