492b7175b30baca3dab5bd4dcf6249ecc166dafd
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-13 20:05:38 +0900

    feat : Auditing 활성화

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,7 +2,9 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication // 스프링 부트의 자동 설정 (항상 프로젝트 최상단)
 public class Application {
     public static void main(String[] args) {
