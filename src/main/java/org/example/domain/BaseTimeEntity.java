1d67a34db980be158a0e4c71ee36aa3e6c1a13b6
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-13 20:03:49 +0900

    feat : Auditing 기능 추가

---
+++ src/main/java/org/example/domain/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package org.example.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass // JPA Entity 클래스들이 BaseTimeEntity를 상속할 경우 필드들도 칼럼으로 인식
+@EntityListeners(AuditingEntityListener.class) // Auditing 기능 포함 (Spring Data JPA에서 시간에 대해서 자동으로 값을 넣어주는 기능)
+public class BaseTimeEntity {
+
+    @CreatedDate // Entity가 생성되어 저장될 때 시간이 자동 저장
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate // 조회한 Entity의 값을 변경할 때 시간이 자동 저장
+    private LocalDateTime modifiedDate;
+}
