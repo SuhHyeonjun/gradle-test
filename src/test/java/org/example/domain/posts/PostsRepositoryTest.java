d0023dfc9e558b4d97c447ef4543b95c3344605e
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-13 23:49:27 +0900

    chore : Auditing 테스트

--- src/test/java/org/example/domain/posts/PostsRepositoryTest.java
+++ src/test/java/org/example/domain/posts/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package org.example.domain.posts;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
 import org.junit.Test;
@@ -44,5 +45,27 @@ public class PostsRepositoryTest {
         Posts posts = postsLists.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime date = LocalDateTime.of(2023,2,1,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("cotent")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>> createDate : " + posts.getCreatedDate() + ", modifiedDate : " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(date); // posts.getCreatedDate()이 date 값 보다 이후 시간인지 비교
+        assertThat(posts.getModifiedDate()).isAfter(date);
     }
 }
