a7660705aecd3f1b72b1ce87fe487d94d16b59d3
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-16 20:12:50 +0900

    feat : User Repo

---
+++ src/main/java/org/example/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package org.example.domain.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 소셜 로그인으로 반환되는 값 중 email을 통해 이미 생성된 사용자인지 처음 가입되는 사용자인지 판단하기 위한 메서드
+    Optional<User> findByEmail(String email);
+}
