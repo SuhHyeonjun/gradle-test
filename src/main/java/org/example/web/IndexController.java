057c9355a4f7d3021c2fd8bb2a89b8a4ca8687b1
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-16 20:12:03 +0900

    feat : 로그인 세션 추가

--- src/main/java/org/example/web/IndexController.java
+++ src/main/java/org/example/web/IndexController.java
@@ -1,6 +1,8 @@
 package org.example.web;
 
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.example.config.auth.dto.SessionUser;
 import org.example.domain.posts.Posts;
 import org.example.service.posts.PostsService;
 import org.example.web.dto.PostsResponseDto;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // 머스테치 스타터가 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자를 자동으로 지정해준다.
     // Ex : 앞의 경로는 src/main/resourses/templates로, 뒤의 파일 확장자는 mustache
@@ -22,7 +25,14 @@ public class IndexController {
     // 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에전달.
     @GetMapping("/")
     public String index(Model model) {
+        // Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다. 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
+        // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // 세션에 저장된 값이 있을 때만 model에 userName 등록 (null이면 로그인 버튼이 보임)
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
