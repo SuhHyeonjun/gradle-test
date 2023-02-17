df8cecc6e08f606ef6b220834667eb2b7afc7f68
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-17 12:41:21 +0900

    refactor : @LoginUser 사용으로 모든 컨트롤러 세션 정보 사용가능하도록 수정

--- src/main/java/org/example/web/IndexController.java
+++ src/main/java/org/example/web/IndexController.java
@@ -2,6 +2,7 @@ package org.example.web;
 
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.example.config.auth.LoginUser;
 import org.example.config.auth.dto.SessionUser;
 import org.example.domain.posts.Posts;
 import org.example.service.posts.PostsService;
@@ -23,12 +24,15 @@ public class IndexController {
     // 여기선 retrun "index"를 반환하므로 "/"엔드포인트로 접속시 /index.mustache로 전환되어 View Resolver가 처리한다.
     // Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
     // 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에전달.
-    @GetMapping("/")
-    public String index(Model model) {
+    @GetMapping("/") // "@LoginUser SessionUser" user로 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있도록 추가
+    public String index(Model model, @LoginUser SessionUser user) {
         // Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다. 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
-        // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        /*
+        반복 코드 사용 제거를 위해 코드 수정 후, 아래 코드를 주석 처리
+        * CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
+        *  SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        */
         // 세션에 저장된 값이 있을 때만 model에 userName 등록 (null이면 로그인 버튼이 보임)
         if (user != null) {
             model.addAttribute("userName", user.getName());
